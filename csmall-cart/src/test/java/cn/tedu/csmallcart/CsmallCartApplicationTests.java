f817365ee9401d7eec14dbefefab97ee30d5de79
chen <602042954@qq.com>
2023-04-18 10:11:45 +0800

    commons

---
+++ csmall-cart/src/test/java/cn/tedu/csmallcart/CsmallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.csmallcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
