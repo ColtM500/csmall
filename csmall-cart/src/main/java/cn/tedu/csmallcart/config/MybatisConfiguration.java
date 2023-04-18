556b31bc43882a241467277d2bc3fdf711f0c413
chen <602042954@qq.com>
2023-04-18 12:00:19 +0800

    application-dev.yml + cart's config

---
+++ csmall-cart/src/main/java/cn/tedu/csmallcart/config/MybatisConfiguration.java
@@ -0,0 +1,10 @@
+package cn.tedu.csmallcart.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//Mybatis框架要求扫描指定的包 包中生成操作数据库接口的实现类
+@MapperScan("cn.tedu.csmallcart.mapper")
+public class MybatisConfiguration {
+}
---
+++ csmall-cart/src/main/java/cn/tedu/csmallcart/service/ICartService.java
@@ -0,0 +1,12 @@
+package cn.tedu.csmallcart.service;
+
+import cn.tedu.csmall.commons.pojo.cart.dto.CartAddDTO;
+
+public interface ICartService {
+
+    //新增购物车商品的业务逻辑层
+    void cartAdd(CartAddDTO cartAddDTO);
+
+    //删除购物车商品的业务逻辑层
+    void deleteUserCart(String userId, String commodityCode);
+}
